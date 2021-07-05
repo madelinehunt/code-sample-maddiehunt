26dae5e059af79eda076d4f70cef99dcb46bd22e
Nathan Hunt <nhunt@N-Hax.local>
2021-07-05 10:33:52 -0700

    updating Promise for write-data handler

--- src/write-data.js
+++ src/write-data.js
@@ -2,18 +2,8 @@ const AWS = require('aws-sdk');
 const config = require('../config/local-config');
 const chai = require('chai');
 const assert = chai.assert;
-//
-// AWS.config.update(config.db);
-const doc_client = new AWS.DynamoDB.DocumentClient(config.db);
-//
-// const dynamodb = new AWS.DynamoDB.DocumentClient({
-//   apiVersion: '2012-08-10',
-//   endpoint: new AWS.Endpoint('http://localhost:8000'),
-//   region: 'us-west-2',
-//   // what could you do to improve performance?
-// });
 
-const tableName = config.tableName;
+const doc_client = new AWS.DynamoDB.DocumentClient(config.db);
 
 /**
  * The entry point into the lambda
@@ -26,43 +16,76 @@ const tableName = config.tableName;
  * @param {string} event.studentLastName
  * @param {string} event.studentGrade
  */
-exports.handler = async (event) => {
-  // TODO use the AWS.DynamoDB.DocumentClient to save the 'SchoolStudent' record
-  // The 'SchoolStudents' table key is composed of schoolId (partition key) and studentId (range key).
+const validate = (event) =>{
   assert.exists(event, 'event is undefined!');
   assert.exists(event.schoolId, 'school ID is required!');
   assert.exists(event.studentId, 'studentId is required!');
   assert.exists(event.studentFirstName, 'studentFirstName is required!');
   assert.exists(event.studentLastName, 'studentLastName is required!');
   assert.exists(event.studentGrade, 'studentGrade is required!');
+}
 
-  // var record = {
-  //   TableName: config.tableName,
-  //   Item: {
-  //     'schoolId': event.schoolId,
-  //     'studentId': event.studentId,
-  //     'studentFirstName': event.studentFirstName,
-  //     'studentLastName': event.studentLastName,
-  //     'studentGrade': event.studentGrade,
-  //   },
-  // };
+exports.handler = (event) => {
+  validate(event);
+
   var record = {
     TableName: config.tableName,
     Item: event,
   };
 
-  let req = await doc_client.put(record, function(err, data){
-    if (err) {
-      console.error("error");
-      console.error(err)
-      console.error(JSON.stringify(err, null, 2));
-      return false;
-    } else {
-      console.log("Succeeded");
-    }
-  }).promise().then(()=>{
-    return new Promise(function(resolve, reject) {
-      resolve("OK");
+  // let fetch = async () => {
+  //   let req = await doc_client.put(record, (err, data) => {
+  //     if (err) {
+  //       console.error("error");
+  //       console.error(err)
+  //     } else {
+  //       // console.log("Succeeded");
+  //     }
+  //   });
+  //   return req;
+  // };
+  //
+  // let response = fetch().then((req)=>{
+  //   // console.log(req);
+  // });
+  //
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(()=>{resolve("OK")}, 30)
+  // });
+  const promise =  new Promise((resolve, reject) => {
+    let req = doc_client.put(record, (err, data) => {
+      if (err) {
+        console.error("error");
+        console.error(err)
+        reject(err);
+      } else {
+        console.log("Succeeded");
+        resolve(data);
+      }
     });
   });
+  return promise;
+};
+
+exports.async_handler = async (event) => {
+  validate(event);
+
+  var record = {
+    TableName: config.tableName,
+    Item: event,
+  };
+
+  const promise =  new Promise(async (resolve, reject) => {
+    let req = await doc_client.put(record, (err, data) => {
+      if (err) {
+        console.error("error");
+        console.error(err)
+        reject(err);
+      } else {
+        console.log("Succeeded");
+        resolve(data);
+      }
+    });
+  });
+  return promise;
 };
